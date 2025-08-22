3d8fe8b75233e01f8356e2030cda413eedf3e69b
kobayashi5829 <jimonjitou5884@gmail.com>
2025-08-23 00:33:14 +0900

    initial commit

---
+++ kobapay/settings_dev.py
@@ -0,0 +1,42 @@
+from .settings_common import *
+
+# ロギング設定
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # paycashアプリケーションが利用するロガー
+        'paycash': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev',
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%lineno)d',
+                '%(message)s',
+            ])
+        },
+    },
+}
