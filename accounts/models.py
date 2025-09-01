2b8e4195a5b1ebffde8556d288e9f81e871544a2
kobayashi5829 <jimonjitou5884@gmail.com>
2025-09-02 00:47:34 +0900

    ホーム画面

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    total_amount = models.IntegerField(blank=True, null=False, default=0)
+    
     class Meta:
         verbose_name_plural = 'CustomUser'
