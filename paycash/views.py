324b8ccd914863109e4db22e8ef3bafefac0a29c
kobayashi5829 <jimonjitou5884@gmail.com>
2025-09-12 14:50:02 +0900

    エラーページの追加

--- paycash/views.py
+++ paycash/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
 from .models import Deal
 from .forms import DealForm
 
