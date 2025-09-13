6d41e832f43857fa74f87d99b124622755ac24ee
kobayashi5829 <jimonjitou5884@gmail.com>
2025-09-13 11:44:10 +0900

    本番環境の設定ファイルの追加

---
+++ kobapay/settings.py
@@ -0,0 +1,54 @@
+from .settings_common import *
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+
+STATIC_ROOT = '/user/share/nginx/html/static'
+
+# ロギング設定
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        # paycashアプリケーションが利用するロガー
+        'paycash': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'formatter': 'prod',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 7,
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'prod': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d',
+                '%(message)s',
+            ])
+        },
+    },
+}
